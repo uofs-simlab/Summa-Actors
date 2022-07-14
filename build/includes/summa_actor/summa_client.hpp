c656e79111dc54dcb4e4d2747a6c4f3db7c13d8c
kyle.c.klenk@gmail.com <kyle.c.klenk@gmail.com>
2022-07-14 13:13:59 -0600

    fixed config file issue

--- build/includes/summa_actor/summa_client.hpp
+++ build/includes/summa_actor/summa_client.hpp
@@ -4,18 +4,19 @@
 #include "caf/io/all.hpp"
 
 #include <string>
+#include <optional>
 
 namespace caf {
 
 struct summa_client_state {
     strong_actor_ptr current_server;
     std::string hostname;
-    std::string config_path;
+    std::optional<std::string> config_path;
     actor summa_actor_ref;
     int batch_id;
     int client_id; // id held by server
 };
-behavior summa_client(stateful_actor<summa_client_state>* self);
+behavior summa_client(stateful_actor<summa_client_state>* self, std::optional<std::string> config_path);
 behavior unconnected(stateful_actor<summa_client_state>*);
 void connecting(stateful_actor<summa_client_state>*, const std::string& host, uint16_t port);
 behavior running(stateful_actor<summa_client_state>*, const actor& summa_server);
--- build/run_script.sh
+++ build/run_script.sh
@@ -3,4 +3,4 @@ cd /Summa-Actors/build
 make -f /Summa-Actors/build/makefile-container all
 export LD_LIBRARY_PATH=/Summa-Actors/bin
 cd /Summa-Actors/bin
-./summaMain -s -c ../config/
+./summaMain -c /gladwell/kck540/Summa-Distributed/herschel/
