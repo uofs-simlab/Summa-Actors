33c0c78b49ab0f95d91ddeb6c4e0371c8f5e99c9
Kyle <kyle.c.klenk@gmail.com>
2022-08-14 20:52:12 -0600

    Celia_test produces the correct output

--- utils/containers/docker/launch_docker_container.sh
+++ utils/containers/docker/launch_docker_container.sh
@@ -2,7 +2,8 @@
 
 export PROJECT_DIR=/Users/kyleklenk/SUMMA-Projects/Summa-Actors
 export NA_TEST=/home/local/kck540/NA_Summa_Test
-export SUMMA=/home/local/kck540/SUMMA-Projects/summa-reference/summa
-docker run -d -it --name SUMMA-Actors --mount type=bind,source=${PROJECT_DIR},target=/Summa-Actors summa-actors:latest
+export SUMMA=/Users/kyleklenk/SUMMA-Projects/summa
+docker run -d -it --name SUMMA-Actors --mount type=bind,source=${PROJECT_DIR},target=/Summa-Actors \
+    --mount type=bind,source=${SUMMA},target=/SUMMA summa-actors:latest
     # --mount type=bind,source=${NA_TEST},target=/NA_Test \
-    # --mount type=bind,source=${SUMMA},target=/SUMMA \
+    # \
