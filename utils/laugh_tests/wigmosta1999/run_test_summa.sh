b7b00a37e453b7482d0b5487e639f222b4624162
Kyle <kyle.c.klenk@gmail.com>
2022-08-16 13:33:18 -0600

    passed all laugh_tests excluding wigmosta

---
+++ utils/laugh_tests/wigmosta1999/run_test_summa.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+/SUMMA/bin/summa.exe -g 1 1 -m /Summa-Actors/utils/laugh_tests/miller1998/settings/summa_fileManager_verify_millerClay.txt
+/SUMMA/bin/summa.exe -g 1 1 -m /Summa-Actors/utils/laugh_tests/miller1998/settings/summa_fileManager_verify_millerLoam.txt
+/SUMMA/bin/summa.exe -g 1 1 -m /Summa-Actors/utils/laugh_tests/miller1998/settings/summa_fileManager_verify_millerSand.txt
