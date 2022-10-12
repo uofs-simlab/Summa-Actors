83816f46a46da3d101d38dd3702790c90e3b5391
KyleKlenk <kyle.c.klenk@gmail.com>
2022-10-12 15:19:14 -0600

    get rid of make for the clients

--- config/summa-distributed/launch_remote_clients.sh
+++ config/summa-distributed/launch_remote_clients.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-ssh kck540@simlab05.usask.ca /home/kck540/Summa-Actors/config/summa-distributed/launch_client.sh
+ssh kck540@simlab01.usask.ca /home/kck540/Summa-Actors/config/summa-distributed/launch_client.sh &
+ssh kck540@simlab05.usask.ca /home/kck540/Summa-Actors/config/summa-distributed/launch_client.sh &
+ssh kck540@simlab17.usask.ca /home/kck540/Summa-Actors/config/summa-distributed/launch_client.sh &
+ssh kck540@simlab18.usask.ca /home/kck540/Summa-Actors/config/summa-distributed/launch_client.sh &
