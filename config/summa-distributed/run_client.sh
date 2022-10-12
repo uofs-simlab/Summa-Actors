55ee523c74bab3998d53c44dbcc4b5f483353216
KyleKlenk <kyle.c.klenk@gmail.com>
2022-10-12 13:57:53 -0600

    changed which python program is called in run_client.sh

--- config/summa-distributed/run_client.sh
+++ config/summa-distributed/run_client.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export LD_LIBRARY_PATH=/Summa-Actors/bin/:/usr/local/lib
 cd /Summa-Actors/config/
-python3 configuration.py "simlab03" 4444
+python3 create_client_config.py "simlab03" 4444
 cd /Summa-Actors/build
 # make
 /Summa-Actors/bin/summaMain -c /Summa-Actors/config/Summa_Actors_Settings.json
