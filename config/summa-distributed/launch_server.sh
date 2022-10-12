e474377bcb73529245eacb73710d17814c848353
KyleKlenk <kyle.c.klenk@gmail.com>
2022-10-12 13:38:15 -0600

    created script for launching client

--- config/summa-distributed/launch_server.sh
+++ config/summa-distributed/launch_server.sh
@@ -4,4 +4,5 @@
 nohup apptainer exec --bind /gladwell:/gladwell \
      --bind /scratch:/scratch \
      --bind /home/kck540/Summa-Actors:/Summa-Actors \
-     /gladwell/kck540/container_files/summa_actors.sif /Summa-Actors/config/summa-distributed/run_server.sh 2>&1 > /scratch/kck540/Summa_Distributed_test/logs/nohup.out &
+     /gladwell/kck540/container_files/summa_actors.sif \
+     /Summa-Actors/config/summa-distributed/run_server.sh 2>&1 > /scratch/kck540/Summa_Distributed_test/logs/`hostname`-server.out &
---
+++ config/summa-distributed/run_cleint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+PORT = 4444
+HOSTNAME = "simlab03"
+
+export LD_LIBRARY_PATH=/Summa-Actors/bin/:/usr/local/lib
+cd /Summa-Actors/config/
+python3 configuration.py $HOSTNAME $PORT
+cd /Summa-Actors/build
+make
+/Summa-Actors/bin/summaMain -c /Summa-Actors/config/Summa_Actors_Settings.json
