f8401410b297cb9b3a7b3ace6dcfe5517230b661
KyleKlenk <kyle.c.klenk@gmail.com>
2022-10-12 13:56:29 -0600

    re-adjusted run_client.sh

--- config/summa-distributed/run_client.sh
+++ config/summa-distributed/run_client.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-export PORT = 4444
-export HOSTNAME = "simlab03"
-
 export LD_LIBRARY_PATH=/Summa-Actors/bin/:/usr/local/lib
 cd /Summa-Actors/config/
-python3 configuration.py $HOSTNAME $PORT
+python3 configuration.py "simlab03" 4444
 cd /Summa-Actors/build
 # make
 /Summa-Actors/bin/summaMain -c /Summa-Actors/config/Summa_Actors_Settings.json
