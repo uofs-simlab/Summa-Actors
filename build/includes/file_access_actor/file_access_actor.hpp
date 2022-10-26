911ec85bbd7d3d47e4de71b5a08d32f9b160d12d
KyleKlenk <kyle.c.klenk@gmail.com>
2022-10-26 00:09:16 +0000

    moved files to separate fortran from cpp within the file_access_actor
    added the start of subroutine for the file_access_actor to send attribute data

--- build/includes/file_access_actor/file_access_actor.hpp
+++ build/includes/file_access_actor/file_access_actor.hpp
@@ -27,6 +27,17 @@ struct file_access_state {
     std::vector<Forcing_File_Info> forcing_file_list; // list of steps in file
     std::vector<bool> outputFileInitHRU;
 
+
+    // vector of handles for attributes
+    std::vector<void *> type_struct_handles;
+    std::vector<void *> id_struct_handles;
+    std::vector<void *> attr_struct_handles;
+
+    // vector of handles for parameters
+    std::vector<void *> mpar_struct_handles;
+    std::vector<void *> bpar_struct_handles;
+
+
      // Timing Variables
     TimingInfo file_access_timing;
 };
@@ -37,5 +48,9 @@ void initalizeFileAccessActor(stateful_actor<file_access_state>* self);
 int writeOutput(stateful_actor<file_access_state>* self, int indxGRU, int indxHRU, int numStepsToWrite, int returnMessage, caf::actor actorRef);
 int readForcing(stateful_actor<file_access_state>* self, int currentFile);
 int write(stateful_actor<file_access_state>* self, int listIndex);
+
+// Read in the attributes for all HRUs that are in the run-domain
+void readAttributes(stateful_actor<file_access_state>* self); 
+
 
 } // end namespace
