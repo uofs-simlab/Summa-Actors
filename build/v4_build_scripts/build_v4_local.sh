178dcd2fe09b3240994d36c0c8674cabea91f555
KyleKlenk <kyle.c.klenk@gmail.com>
2024-06-17 12:48:11 -0600

    Add CMakeLists.txt for Summa-Actors: This allows the summa-actors program to separate its individual files from SUMMA. Before compilation was controlled by the summa directory and required frequent pull requests for changes in Summa-Actors that did not affect SUMMA. These changes should allow files to change in Summa-Actors without requiring a pull request to Summa

--- build/v4_build_scripts/build_v4_local.sh
+++ build/v4_build_scripts/build_v4_local.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+export CMAKE_PREFIX_PATH="/opt/homebrew/opt/openblas/"
   
-cmake -B ./cmake_build -S ../summa/build/. -DUSE_ACTORS=ON -DUSE_SUNDIALS=ON
+cmake -B ./cmake_build -S .. -DUSE_SUNDIALS=ON
 cmake --build ./cmake_build --target all -j
