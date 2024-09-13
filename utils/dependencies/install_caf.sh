b59b61ff45cba5e14e4716b5dcf535d4a2c851cd
kyleklenk <kyle.c.klenk@gmail.com>
2024-09-13 15:13:00 -0400

    Add new install script

--- utils/dependencies/install_caf.sh
+++ utils/dependencies/install_caf.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+INSTALL_DIR=$PWD/caf
 
+# Load the necessary modules (cluster dependent below work for anvil)
+# module load gcc/11.2.0 
+# module load netlib-lapack
+# module load netcdf-fortran
 
 wget https://github.com/actor-framework/actor-framework/archive/refs/tags/1.0.0.tar.gz
 tar -xvf 1.0.0.tar.gz
 
-INSTALL_DIR=$PWD/caf
 
 echo "Installing CAF to $INSTALL_DIR"
 
