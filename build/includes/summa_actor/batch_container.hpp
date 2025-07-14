f3ee8407f36fd3b09d564e5ec1b886e2bbdefdb1
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2025-07-14 13:14:02 -0600

    Added a new BatchContainer constructor stub, that will not call assemble batches when used.

--- build/includes/summa_actor/batch_container.hpp
+++ build/includes/summa_actor/batch_container.hpp
@@ -23,6 +23,9 @@ class BatchContainer {
                     int num_hru_per_batch = 0, std::string log_dir = "");
 
 
+    //Initialize BatchContainer -- do not call assembleBatches()
+    BatchContainer(int start_hru = 1,int total_hru_count = 0,std::string log_dir = "");
+
     // returns the size of the batch list
     inline int getBatchesRemaining() {return batches_remaining_;}
     inline int getTotalBatches() { return batch_list_.size();}
