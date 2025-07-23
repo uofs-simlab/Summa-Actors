6b2ff5d9373faf494a883e034bc6bca1416b4262
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2025-07-23 13:29:08 -0600

    Toggled off features that would create and write to output file. Changes were made to step around the error of cannot define output file that crashes summa actors.

--- build/source/file_access_actor/output_buffer.cpp
+++ build/source/file_access_actor/output_buffer.cpp
@@ -161,8 +161,8 @@ const std::optional<WriteOutputReturn*> OutputPartition::writeOutput(
     std::unique_ptr<char[]> message(new char[256]);
     bool write_params = isWriteParams();
     
-    writeOutput_fortran(handle_ncid, num_steps_buffer_, start_gru_, end_gru_, 
-                        write_params, err, &message);
+    //writeOutput_fortran(handle_ncid, num_steps_buffer_, start_gru_, end_gru_, 
+     //                   write_params, err, &message);
     
     // recalculate the number of steps to send to grus
     steps_remaining_ -= num_steps_buffer_;
@@ -192,8 +192,8 @@ const std::optional<WriteOutputReturn*> OutputPartition::writeOutput(
     int err = 0;
     std::unique_ptr<char[]> message(new char[256]);
     bool write_params = isWriteParams();
-    writeOutput_fortran(handle_ncid, num_steps_buffer_, start_gru_, end_gru_, 
-                        write_params, err, &message);
+    //writeOutput_fortran(handle_ncid, num_steps_buffer_, start_gru_, end_gru_, 
+    //                    write_params, err, &message);
     // recalculate the number of steps to send to grus
     steps_remaining_ -= num_steps_buffer_;
     if (steps_remaining_ < num_steps_buffer_) {
